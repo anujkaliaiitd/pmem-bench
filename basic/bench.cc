62eed9c7f3acf46bb6f7ed863a32ef245e5444c5
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-07-30 17:00:12 -0700

    save work

--- basic/bench.cc
+++ basic/bench.cc
@@ -7,7 +7,31 @@
 #include "../common.h"
 
 static constexpr size_t kFileSizeGB = 64;  // The expected file size
-static constexpr size_t kNumIters = 100000;
+static constexpr size_t kNumIters = 1000000;
+
+static constexpr size_t kFileSizeBytes = kFileSizeGB * GB(1);
+
+/// Latency of random reads
+void bench_rand_read_lat(uint8_t *pbuf) {
+  FastRand rand;
+  struct timespec start, end;
+  size_t sum = 0;
+  clock_gettime(CLOCK_REALTIME, &start);
+
+  for (size_t i = 0; i < kNumIters; i++) {
+    // Choose a random byte
+    size_t rand_byte = ((sum % 8) + static_cast<size_t>(rand.next_u32())) * 64;
+
+    sum += pbuf[rand_byte % kFileSizeBytes];
+  }
+
+  clock_gettime(CLOCK_REALTIME, &end);
+  double tot_ns = (end.tv_sec - start.tv_sec) * 1000000000.0 +
+                  (end.tv_nsec - start.tv_nsec);
+
+  printf("Latency of random reads = %.2f ns. Sum = %zu\n", tot_ns / kNumIters,
+         sum);
+}
 
 /// Latency of persisting to the same byte in a file. Useful for timestamps etc.
 void bench_write_lat_byte(uint8_t *pbuf) {
@@ -29,7 +53,7 @@ void bench_write_lat_byte(uint8_t *pbuf) {
 
 /// Bandwidth of large writes
 void bench_write_sequential(uint8_t *pbuf) {
-  void *dram_src_buf = malloc(kFileSizeGB * GB(1));
+  void *dram_src_buf = malloc(kFileSizeBytes);
   rt_assert(dram_src_buf != nullptr);
 
   for (size_t copy_GB = 1; copy_GB <= 8; copy_GB *= 2) {
@@ -57,12 +81,18 @@ int main() {
       pmem_map_file("/mnt/pmem12/src.txt", 0 /* length */, 0 /* flags */, 0666,
                     &mapped_len, &is_pmem));
 
+  rt_assert(reinterpret_cast<size_t>(pbuf) % 4096 == 0,
+            "Mapped buffer isn't page-aligned");
   rt_assert(pbuf != nullptr,
             "pmem_map_file() failed. " + std::string(strerror(errno)));
   rt_assert(mapped_len == kFileSizeGB * GB(1),
             "Incorrect file size " + std::to_string(mapped_len));
   rt_assert(is_pmem == 1, "File is not pmem");
 
+  printf("Warming up for around 1 second.\n");
+  nano_sleep(1000000000, 3.0);  // Assume TSC frequency = 3 GHz
+
+  bench_rand_read_lat(pbuf);
   bench_write_lat_byte(pbuf);
   bench_write_sequential(pbuf);
 
--- common.h
+++ common.h
@@ -2,12 +2,40 @@
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
+#include <random>
 #include <stdexcept>
 #include <string>
 
 #define KB(x) (static_cast<size_t>(x) << 10)
 #define MB(x) (static_cast<size_t>(x) << 20)
 #define GB(x) (static_cast<size_t>(x) << 30)
+
+class SlowRand {
+  std::random_device rand_dev;  // Non-pseudorandom seed for twister
+  std::mt19937_64 mt;
+  std::uniform_int_distribution<uint64_t> dist;
+
+ public:
+  SlowRand() : mt(rand_dev()), dist(0, UINT64_MAX) {}
+
+  inline uint64_t next_u64() { return dist(mt); }
+};
+
+class FastRand {
+ public:
+  uint64_t seed;
+
+  /// Create a FastRand using a seed from SlowRand
+  FastRand() {
+    SlowRand slow_rand;
+    seed = slow_rand.next_u64();
+  }
+
+  inline uint32_t next_u32() {
+    seed = seed * 1103515245 + 12345;
+    return static_cast<uint32_t>(seed >> 32);
+  }
+};
 
 /// Check a condition at runtime. If the condition is false, throw exception.
 static inline void rt_assert(bool condition, std::string throw_str, char *s) {
@@ -25,4 +53,22 @@ static inline void rt_assert(bool condition, std::string throw_str) {
 /// This is faster than rt_assert(cond, str) as it avoids string construction.
 static inline void rt_assert(bool condition) {
   if (unlikely(!condition)) throw std::runtime_error("Error");
+}
+
+/// Return the TSC
+static inline size_t rdtsc() {
+  uint64_t rax;
+  uint64_t rdx;
+  asm volatile("rdtsc" : "=a"(rax), "=d"(rdx));
+  return static_cast<size_t>((rdx << 32) | rax);
+}
+
+/// An alias for rdtsc() to distinguish calls on the critical path
+static const auto &dpath_rdtsc = rdtsc;
+
+static void nano_sleep(size_t ns, double freq_ghz) {
+  size_t start = rdtsc();
+  size_t end = start;
+  size_t upp = static_cast<size_t>(freq_ghz * ns);
+  while (end - start < upp) end = rdtsc();
 }
