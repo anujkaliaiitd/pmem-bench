3b87cfdc81e480e619e7dc7b55e25fd3383ffd87
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-11 13:24:15 -0700

    add hdr histogram to common.h

--- common.h
+++ common.h
@@ -156,4 +156,27 @@ static double ns_since(const struct timespec &t0) {
   return (t1.tv_sec - t0.tv_sec) * 1000000000.0 + (t1.tv_nsec - t0.tv_nsec);
 }
 
-typedef unsigned long long ticks;
+#include <hdr/hdr_histogram.h>
+// A wrapper for hdr_histogram that supports floating point values with
+// magnified precision. A floating point record x is inserted as x * AMP.
+template <size_t AMP>
+class HdrHistogramAmp {
+ public:
+  HdrHistogramAmp(int64_t min, int64_t max, uint32_t precision) {
+    int ret = hdr_init(min * AMP, max * AMP, precision, &hist);
+    rt_assert(ret == 0);
+  }
+
+  ~HdrHistogramAmp() { hdr_close(hist); }
+
+  inline void record_value(double v) { hdr_record_value(hist, v * AMP); }
+
+  void percentile(double p) { return hdr_value_at_percentile(hist, p) * AMP; }
+
+  void reset() { hdr_reset(hist); }
+
+  hdr_histogram *get_raw_hist() { return hist; }
+
+ private:
+  hdr_histogram *hist = nullptr;
+};
