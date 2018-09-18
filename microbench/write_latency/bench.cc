edbd1738bb76139fe7ea4cf3887af556e55a1159
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-17 20:33:34 -0700

    simplify write_latency bench

--- microbench/write_latency/bench.cc
+++ microbench/write_latency/bench.cc
@@ -5,14 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "../../common.h"
+#include <x86intrin.h>
+#include <algorithm>
+#include <vector>
 
-static constexpr size_t kWriteSize = 1024;
+static constexpr size_t kWriteSize = 512;
 static constexpr size_t kNumIters = 1000000;
-
-static constexpr size_t kMinAEPLatCycles = 1;
-static constexpr size_t kMaxAEPLatCycles = MB(1024);
-static constexpr size_t kAEPLatPrecision = 2;
 
 int main() {
   const uint8_t data[kWriteSize] = {0};
@@ -21,40 +19,45 @@ int main() {
   int is_pmem;
   uint8_t *pbuf = reinterpret_cast<uint8_t *>(
       pmem_map_file("/dev/dax0.0", 0, 0, 0666, &mapped_len, &is_pmem));
-
   assert(pbuf != nullptr);
   assert(mapped_len >= kWriteSize * kNumIters);
 
-  const double freq_ghz = measure_rdtsc_freq();
-  HdrHistogram hist(kMinAEPLatCycles, kMaxAEPLatCycles, kAEPLatPrecision);
   size_t offset = 0;
+  std::vector<size_t> latency_vec;
+  latency_vec.reserve(kNumIters);
 
   for (size_t msr = 0; msr < 10; msr++) {
-    hist.reset();
+    latency_vec.clear();
     struct timespec bench_start;
     clock_gettime(CLOCK_REALTIME, &bench_start);
 
     // Real work
     for (size_t i = 0; i < kNumIters; i++) {
-      size_t start_tsc = rdtscp();
-      mfence();
-
+      size_t start_tsc = __rdtsc();
+      _mm_mfence();
       pmem_memcpy_persist(&pbuf[offset], data, kWriteSize);
+      _mm_mfence();
 
-      mfence();
-      hist.record_value(rdtscp() - start_tsc);
+      latency_vec.push_back(__rdtsc() - start_tsc);
 
       offset += kWriteSize;
       if (offset + kWriteSize >= mapped_len) offset = 0;
     }
 
-    const double bench_seconds = sec_since(bench_start);
+    struct timespec bench_end;
+    clock_gettime(CLOCK_REALTIME, &bench_end);
+    double bench_seconds =
+        (bench_end.tv_sec - bench_start.tv_sec) +
+        (bench_end.tv_nsec - bench_start.tv_nsec) / 1000000000.0;
+
     printf("Throughput of writes = %.2f M ops/s, %.2f GB/s\n",
            kNumIters / (bench_seconds * 1000000),
            kNumIters * kWriteSize / (bench_seconds * 1000000000));
-    printf("Latency (ns): 50 %.1f, 99 %.1f, 99.9 %.1f\n",
-           hist.percentile(50) / freq_ghz, hist.percentile(99) / freq_ghz,
-           hist.percentile(99.9) / freq_ghz);
+
+    std::sort(latency_vec.begin(), latency_vec.end());
+    printf("Latency (cycles): median %zu, 99%% %zu, 99.9%% %zu\n",
+           latency_vec.at(kNumIters * .5), latency_vec.at(kNumIters * .99),
+           latency_vec.at(kNumIters * .999));
   }
 
   pmem_unmap(pbuf, mapped_len);
