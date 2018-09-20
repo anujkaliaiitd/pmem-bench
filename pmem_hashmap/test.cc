813dad549d7dd49915c67248130a21b913013293
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-19 20:52:21 -0700

    save

--- pmem_hashmap/test.cc
+++ pmem_hashmap/test.cc
@@ -14,6 +14,19 @@ TEST(InsertTest, Test) {
 
   success = hashmap.insert(3, 3);
   ASSERT_FALSE(success);
+
+  size_t val = 0;
+  success = hashmap.get(1, val);
+  ASSERT_EQ(val, 1);
+  ASSERT_TRUE(success);
+
+  success = hashmap.get(2, val);
+  ASSERT_EQ(val, 2);
+  ASSERT_TRUE(success);
+
+  success = hashmap.get(3, val);
+  ASSERT_EQ(val, 2);
+  ASSERT_FALSE(success);
 }
 
 int main(int argc, char **argv) {
