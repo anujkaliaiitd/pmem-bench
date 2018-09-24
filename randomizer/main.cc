f1b13525564dcf4cb2295e145b13bc645dbdbefe
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-23 18:19:22 -0700

    add a randomizer

---
+++ randomizer/main.cc
@@ -0,0 +1,52 @@
+// This can be used to write random contents to a pmem file so that later
+// experiments don't benefit from any crazy value prediction of a zeroed file.
+
+#include "../common.h"
+#include <libpmem.h>
+#include <pcg/pcg_random.hpp>
+
+static constexpr const char *kPmemFile = "/dev/dax0.0";
+static constexpr size_t kPmemFileSizeGB = 1024;  // The expected file size
+static constexpr size_t kPmemFileSize = kPmemFileSizeGB * GB(1);
+static constexpr size_t kRandTemplateSz = GB(32);
+
+int main(int, char **) {
+  uint8_t *pbuf;
+  size_t mapped_len;
+  int is_pmem;
+
+  pbuf = reinterpret_cast<uint8_t *>(pmem_map_file(
+      kPmemFile, 0 /* length */, 0 /* flags */, 0666, &mapped_len, &is_pmem));
+
+  rt_assert(pbuf != nullptr,
+            "pmem_map_file() failed. " + std::string(strerror(errno)));
+  rt_assert(mapped_len >= kPmemFileSize,
+            "pmem file too small " + std::to_string(mapped_len));
+  rt_assert(reinterpret_cast<size_t>(pbuf) % 4096 == 0,
+            "Mapped buffer isn't page-aligned");
+  rt_assert(is_pmem == 1, "File is not pmem");
+
+  printf("Generating random contents\n");
+  pcg64_fast pcg(pcg_extras::seed_seq_from<std::random_device>{});
+  size_t *rand_buf = reinterpret_cast<size_t *>(malloc(kRandTemplateSz));
+  for (size_t i = 0; i < kRandTemplateSz / sizeof(size_t); i++) {
+    rand_buf[i] = pcg();
+  }
+
+  printf("Writing random contents to the whole file.\n");
+  rt_assert(kPmemFileSize % kRandTemplateSz == 0);
+
+  for (size_t i = 0; i < kPmemFileSize; i += kRandTemplateSz) {
+    struct timespec start;
+    clock_gettime(CLOCK_REALTIME, &start);
+    pmem_memcpy_persist(&pbuf[i], rand_buf, kRandTemplateSz);
+    printf("Fraction complete = %.2f. Took %.3f sec for %zu GB.\n",
+           (i + 1) * 1.0 / kPmemFileSize, sec_since(start),
+           kRandTemplateSz / GB(1));
+  }
+
+  printf("Done writing.\n");
+
+  pmem_unmap(pbuf, mapped_len);
+  exit(0);
+}
