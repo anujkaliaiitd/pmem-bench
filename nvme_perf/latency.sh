26386b9f2e32d3ef52b1e7bfb9b459bff6ea7bfa
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-23 18:19:59 -0700

    improve nvme latency script to also print averages

--- nvme_perf/latency.sh
+++ nvme_perf/latency.sh
@@ -1,5 +1,6 @@
 perf_exe="/home/akalia/sandbox/spdk/examples/nvme/perf/perf"
 
+rm -f tmpout_*
 rm -rf final_out
 touch final_out
 
@@ -7,6 +8,9 @@ touch final_out
 bench=read       # Sequential reads
 bench=randwrite  # Random writes
 bench=write      # Sequential writes
+bench=randread   # Random reads
+
+echo "size us_avg us_median us_999 us_99" >> final_out
 
 for ((size = 512; size <= 65536; size *= 2)); do
   tmpfile="tmpout_$size"
@@ -19,14 +23,15 @@ for ((size = 512; size <= 65536; size *= 2)); do
   sudo numactl --cpunodebind=1 --membind=1 $perf_exe \
     -q 1 -o $size -w $bench -t 2 -c 0x1000000 -L > $tmpfile
 
+  us_avg=`cat $tmpfile  | grep Total | sed -s 's/  */ /g' | cut -d ' ' -f 5`
   us_median=`cat $tmpfile  | grep "50\.00000"  | tr -d ' ' |  cut -d ":" -f 2 | sed 's/us//g'`
   us_99=`cat $tmpfile  | grep "99\.00000"  | tr -d ' ' |  cut -d ":" -f 2 | sed 's/us//g'`
   us_999=`cat $tmpfile  | grep "99\.90000"  | tr -d ' ' |  cut -d ":" -f 2 | sed 's/us//g'`
 
-  echo $size $us_median $us_99 $us_999
-  echo $size $us_median $us_99 $us_999 >> final_out
+  echo $size $us_avg $us_median $us_999 $us_99
+  echo $size $us_avg $us_median $us_999 $us_99 >> final_out
 done
 
-rm -f tmpout_*
 cat final_out
+rm -f tmpout_*
 rm -rf final_out
