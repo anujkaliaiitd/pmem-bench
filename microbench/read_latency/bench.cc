efd935853ac1ac1d0557dc38a0b2bff2ed590902
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-10-22 22:10:21 -0700

    add pure pointer-chasing read latency measurement

--- microbench/read_latency/bench.cc
+++ microbench/read_latency/bench.cc
@@ -22,6 +22,13 @@ static double ns_since(const struct timespec &t0) {
   return (t1.tv_sec - t0.tv_sec) * 1000000000.0 + (t1.tv_nsec - t0.tv_nsec);
 }
 
+// Used for shuffle-based pointer chain measurement
+struct cacheline_t {
+  cacheline_t *ptr;
+  size_t pad[7];
+};
+static_assert(sizeof(cacheline_t) == 64, "");
+
 int main() {
   size_t mapped_len;
   int is_pmem;
@@ -34,7 +41,7 @@ int main() {
   uint64_t seed = 0xdeadbeef;
   size_t sum = 0;
 
-  while (true) {
+  for (size_t msr = 0; msr < 5; msr++) {
     // Initialize measurement
     struct timespec bench_start;
     clock_gettime(CLOCK_REALTIME, &bench_start);
@@ -52,6 +59,42 @@ int main() {
     printf("Average read latency = %.1f ns, sum = %zu\n", bench_ns / kNumIters,
            sum);
   }
+
+  // Shuffle-based measurement here
+  /*
+   *
+  auto *cl_arr = reinterpret_cast<cacheline_t *>(pbuf);
+  const size_t num_cl = kFileSizeBytes / sizeof(cacheline_t);
+  for (size_t i = 0; i < num_cl; i++) {
+    cl_arr[i].ptr = &cl_arr[i];
+  }
+
+  for (size_t i = num_cl - 1; i >= 1; i--) {
+    if (i % (128 * 1024) == 0) {
+      printf("shuffle progress (left) = %.2f\n", i * 1.0 / num_cl);
+    }
+    size_t j = fastrand(seed) % i;
+    cacheline_t *temp = cl_arr[j].ptr;
+    cl_arr[j].ptr = cl_arr[i].ptr;
+    cl_arr[i].ptr = temp;
+  }
+  cacheline_t *ptr = cl_arr[0].ptr;
+
+  for (size_t msr = 0; msr < 10; msr++) {
+    // Initialize measurement
+    struct timespec bench_start;
+    clock_gettime(CLOCK_REALTIME, &bench_start);
+
+    // Real work
+    for (size_t i = 0; i < kNumIters; i++) {
+      ptr = reinterpret_cast<cacheline_t *>((*ptr).ptr);
+    }
+
+    double bench_ns = ns_since(bench_start);
+    printf("Average read latency = %.1f ns, ptr = %p\n", bench_ns / kNumIters,
+           ptr);
+  }
+  */
 
   pmem_unmap(pbuf, mapped_len);
   exit(0);
