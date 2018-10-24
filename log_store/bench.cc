b95d64c4f3630d336320f4b43bd51cb8a3e8ebef
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-10-23 18:30:50 -0700

    rotating log bench

--- log_store/bench.cc
+++ log_store/bench.cc
@@ -11,6 +11,80 @@ static constexpr const char *kFileName = "/mnt/pmem12/raft_log";
 static constexpr size_t kNumMeasurements = 2;
 static constexpr size_t kNumIters = 1000000;
 
+// Amount of data appended to the log in on iteration
+static constexpr size_t kMaxLogDataSize = 4096;
+
+void counter_only_bench(uint8_t *pbuf) {
+  Counter ctr(pbuf, true /* create a new counter */);
+
+  for (size_t msr = 0; msr < kNumMeasurements; msr++) {
+    struct timespec bench_start;
+    clock_gettime(CLOCK_REALTIME, &bench_start);
+
+    for (size_t i = 0; i < kNumIters; i++) ctr.increment_naive(1);
+
+    double bench_seconds = sec_since(bench_start);
+    printf("Naive counter: %.2f M increments/s\n",
+           kNumIters / (bench_seconds * 1000000));
+  }
+
+  for (size_t msr = 0; msr < kNumMeasurements; msr++) {
+    struct timespec bench_start;
+    clock_gettime(CLOCK_REALTIME, &bench_start);
+
+    for (size_t i = 0; i < kNumIters; i++) ctr.increment_rotate(1);
+
+    double bench_seconds = sec_since(bench_start);
+    printf("Rotating counter: %.2f M increments/s\n",
+           kNumIters / (bench_seconds * 1000000));
+  }
+}
+
+void log_bench(uint8_t *pbuf) {
+  uint8_t source[kMaxLogDataSize] = {0};
+
+  printf("write_bytes naive_GBps rotating_GBps\n");
+
+  // Sweep over write sizes
+  for (size_t write_sz = 64; write_sz <= kMaxLogDataSize; write_sz *= 2) {
+    double naive_GBps, rotating_GBps;
+
+    {
+      // Naive log
+      Log log(pbuf);
+      struct timespec bench_start;
+      clock_gettime(CLOCK_REALTIME, &bench_start);
+
+      for (size_t i = 0; i < kNumIters; i++) {
+        // Modify the source
+        for (size_t j = 0; j < write_sz / 64; j += 64) source[j]++;
+        log.append_naive(source, write_sz);
+      }
+
+      double bench_seconds = sec_since(bench_start);
+      naive_GBps = kNumIters * write_sz / (bench_seconds * GB(1));
+    }
+
+    {
+      // Rotating log
+      Log log(pbuf);
+      struct timespec bench_start;
+      clock_gettime(CLOCK_REALTIME, &bench_start);
+
+      for (size_t i = 0; i < kNumIters; i++) {
+        // Modify the source
+        for (size_t j = 0; j < write_sz / 64; j += 64) source[j]++;
+        log.append_rotating(source, write_sz);
+      }
+
+      double bench_seconds = sec_since(bench_start);
+      rotating_GBps = kNumIters * write_sz / (bench_seconds * GB(1));
+    }
+
+    printf("%zu %.2f %.2f\n", write_sz, naive_GBps, rotating_GBps);
+  }
+}
+
 int main() {
   size_t mapped_len;
   int is_pmem;
@@ -20,40 +94,8 @@ int main() {
   assert(pbuf != nullptr);
   assert(mapped_len >= Counter::get_reqd_space());
 
-  Counter ctr(pbuf, true /* create a new counter */);
-
-  for (size_t msr = 0; msr < kNumMeasurements; msr++) {
-    struct timespec bench_start;
-    clock_gettime(CLOCK_REALTIME, &bench_start);
-
-    for (size_t i = 0; i < kNumIters; i++) ctr.increment_naive();
-
-    double bench_seconds = sec_since(bench_start);
-    printf("Naive: %.2f M increments/s\n",
-           kNumIters / (bench_seconds * 1000000));
-  }
-
-  for (size_t msr = 0; msr < kNumMeasurements; msr++) {
-    struct timespec bench_start;
-    clock_gettime(CLOCK_REALTIME, &bench_start);
-
-    for (size_t i = 0; i < kNumIters; i++) ctr.increment_rotate_no_full_cl();
-
-    double bench_seconds = sec_since(bench_start);
-    printf("Rotating, no full-cacheline writes: %.2f M increments/s\n",
-           kNumIters / (bench_seconds * 1000000));
-  }
-
-  for (size_t msr = 0; msr < kNumMeasurements; msr++) {
-    struct timespec bench_start;
-    clock_gettime(CLOCK_REALTIME, &bench_start);
-
-    for (size_t i = 0; i < kNumIters; i++) ctr.increment_rotate_full_cl();
-
-    double bench_seconds = sec_since(bench_start);
-    printf("Rotating, full-cacheline writes: %.2f M increments/s\n",
-           kNumIters / (bench_seconds * 1000000));
-  }
+  counter_only_bench(pbuf);
+  for (size_t msr = 0; msr < kNumMeasurements; msr++) log_bench(pbuf);
 
   pmem_unmap(pbuf, mapped_len);
   exit(0);
--- log_store/log.h
+++ log_store/log.h
@@ -10,52 +10,79 @@ class Counter {
   static constexpr size_t kNumBuffers = 16;
   static constexpr size_t kBufferSize = 256;
 
-  Counter(uint8_t *pbuf, bool create_new) : pbuf(pbuf) {
+  /**
+   * @brief Construct a counter
+   *
+   * @param pbuf The start address of the counter on persistent memory
+   *
+   * @param create_new If true, the counter is reset to zero. If false, the
+   * counter is initialized using the prior pmem contents.
+   */
+  Counter(uint8_t *pbuf, bool create_new) : ctr_base_addr(pbuf) {
     if (create_new) {
       pmem_memset_persist(pbuf, 0, kNumBuffers * kBufferSize);
     } else {
-      v_value = get_value();
+      size_t cur_max = 0;    // Maximum value among the counters
+      size_t cur_max_i = 0;  // Index of the maximum value
+      for (size_t i = 0; i < kNumBuffers; i++) {
+        size_t *counter_i = reinterpret_cast<size_t *>(&pbuf[i * kBufferSize]);
+        if (*counter_i > cur_max) {
+          cur_max = *counter_i;
+          cur_max_i = i;
+        }
+      }
+
+      v_value = cur_max;
+      buffer_idx = (cur_max_i + 1) % kNumBuffers;
     }
   }
 
-  size_t get_value() const {
-    size_t ret = 0;
-    for (size_t i = 0; i < kNumBuffers; i++) {
-      size_t *counter_i = reinterpret_cast<size_t *>(&pbuf[i * kBufferSize]);
-      ret = std::max(ret, *counter_i);
-    }
-    return ret;
-  }
+  Counter() {}
 
+  /// The amount of contiguous pmem needed for this counter
   static size_t get_reqd_space() { return kNumBuffers * kBufferSize; }
 
   // Increment by always writing to the same location
-  inline void increment_naive() {
-    v_value++;
-    pmem_memcpy_persist(&pbuf[0], &v_value, sizeof(v_value));
+  inline void increment_naive(size_t increment) {
+    v_value += increment;
+    pmem_memcpy_persist(&ctr_base_addr[0], &v_value, sizeof(v_value));
   }
 
   // Increment by writing to rotating locations, but don't do full-cacheline
   // writes
-  inline void increment_rotate_no_full_cl() {
-    v_value++;
-    pmem_memcpy_persist(&pbuf[buffer_idx * kBufferSize], &v_value,
+  inline void increment_rotate(size_t increment) {
+    v_value += increment;
+    pmem_memcpy_persist(&ctr_base_addr[buffer_idx * kBufferSize], &v_value,
                         sizeof(v_value));
     buffer_idx = (buffer_idx + 1) % kNumBuffers;
   }
 
-  // Increment by writing to rotating locations, and do full-cacheline writes
-  inline void increment_rotate_full_cl() {
-    v_value++;
-    size_t cacheline[8];
-    cacheline[0] = v_value;
-
-    pmem_memcpy_persist(&pbuf[buffer_idx * kBufferSize], cacheline, 64);
-    buffer_idx = (buffer_idx + 1) % kNumBuffers;
-  }
-
-  size_t v_value = 0;  // Volatile value
+  size_t v_value = 0;  // Volatile value of the counter
 
   size_t buffer_idx = 0;
-  uint8_t *pbuf = nullptr;
+  uint8_t *ctr_base_addr = nullptr;  // Starting address of the counter on pmem
+};
+
+class Log {
+ public:
+  // Assume pbuf is large enough to never overflow
+  Log(uint8_t *pbuf) {
+    ctr = Counter(pbuf, true /* create_new */);
+    log_base_addr = pbuf + Counter::get_reqd_space();
+  }
+
+  // Append with naive counter incrementing
+  void append_naive(uint8_t *data, size_t data_size) {
+    pmem_memcpy_persist(log_base_addr + ctr.v_value, data, data_size);
+    ctr.increment_naive(data_size);
+  }
+
+  // Append with rotating counter incrementing
+  void append_rotating(uint8_t *data, size_t data_size) {
+    pmem_memcpy_persist(log_base_addr + ctr.v_value, data, data_size);
+    ctr.increment_rotate(data_size);
+  }
+
+  Counter ctr;
+  uint8_t *log_base_addr = nullptr;  // Starting address of log contents on pmem
 };
