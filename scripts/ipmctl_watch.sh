d58409205616c9baced5230575f7d612dc7605b4
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-10-28 10:37:34 -0400

    save

--- scripts/ipmctl_watch.sh
+++ scripts/ipmctl_watch.sh
@@ -10,32 +10,58 @@
 # WriteRequests = Number of 64-byte write requests received on the DDR bus
 #
 # Notes:
-#  * This script uses `calc` to convert hex to decimal, and `xargs` to
+#  * This script uses `printf` to convert hex to decimal, and `xargs` to
 #    trim surrounding whitespaces.
 #  * During idle period, the NVM controller may write to NVM even when no DDR
 #    commands are received. This causes write amplification to be ~100.
+#
 
-media_writes_0=`sudo ipmctl show -dimm 0x0001 -performance MediaWrites \
-  | grep MediaWrites | cut -d'=' -f 2`
+dimms=0x0001 # Single non-interleaved
+dimms=0x0001,0x0011,0x0021,0x0101,0x0111,0x0121  # All DIMMs at socket 0
 
-ddr_writes_0=`sudo ipmctl show -dimm 0x0001 -performance WriteRequests \
-  | grep WriteRequests | cut -d'=' -f 2`
+# Sum metric arg #1 from file watch_out
+sum_from_watch_out() {
+  temp_file=$(mktemp)
+  cat watch_out | grep $1 | cut -d'=' -f 2 > $temp_file
 
+  sum=0
+  while read hex; do
+    dec=`printf "%d\n" $hex`
+    sum=`expr $sum + $dec`
+  done < ${temp_file}
+
+  echo $sum
+  rm ${temp_file}
+}
+
+# Regenerate watch_out
+refresh_watch_out() {
+  rm -f watch_out
+  touch watch_out
+  sudo ipmctl show -dimm $dimms -performance MediaWrites,WriteRequests > watch_out
+}
+
+refresh_watch_out
+media_writes_0=`sum_from_watch_out MediaWrites`
+ddr_writes_0=`sum_from_watch_out WriteRequests`
+
+sleep_seconds=1
 while true; do
+  sleep $sleep_seconds
 
-  media_writes_1=`sudo ipmctl show -dimm 0x0001 -performance MediaWrites \
-    | grep MediaWrites | cut -d'=' -f 2`
-
-  ddr_writes_1=`sudo ipmctl show -dimm 0x0001 -performance WriteRequests \
-    | grep WriteRequests | cut -d'=' -f 2`
+  refresh_watch_out
+  media_writes_1=`sum_from_watch_out MediaWrites`
+  ddr_writes_1=`sum_from_watch_out WriteRequests`
 
   media_writes_delta=`calc $media_writes_1 - $media_writes_0 | xargs`
   ddr_writes_delta=`calc $ddr_writes_1 - $ddr_writes_0 | xargs`
+
+  media_writes_GBs=`python -c "print $media_writes_delta * 64.0 / (1024 * 1024 * 1024 * $sleep_seconds)" | xargs`
+  ddr_writes_GBs=`python -c "print $ddr_writes_delta * 64.0 / (1024 * 1024 * 1024 * $sleep_seconds)" | xargs`
   amp=`calc $media_writes_delta / $ddr_writes_delta | xargs`
 
-  echo "Media writes = $media_writes_delta, DDR writes = $ddr_writes_delta, amplification = $amp"
+  echo "Media writes = $media_writes_delta ($media_writes_GBs GB/s), DDR writes = $ddr_writes_delta ($ddr_writes_GBs GB/s), amplification = $amp"
 
   media_writes_0=$media_writes_1
   ddr_writes_0=$ddr_writes_1
 done
-
